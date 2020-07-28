f974840470e2c53a028537bd7e2530cdf148c8b8
Shane <perrysm98@gmail.com>
2020-07-28 10:26:09 -0500

    After Restart 1 before Restart 2 -- Failure to make List work

--- Tracking App 2.0/Tracking App 2.0/Program.cs
+++ Tracking App 2.0/Tracking App 2.0/Program.cs
@@ -12,114 +12,7 @@ namespace Tracking_App_2._0
         static void Main(string[] args)
         {
 
-            DefaultMenu();
-
-            string exit = Console.ReadLine();
-
-            do
-            {
-                switch (exit)
-                {
-                    case "1":
-                        ActionItem();
-                        break;
-                    case "2":
-                        CrossOut();
-                        break;
-                    case "3":
-                        Skip();
-                        break;
-                    case "4":
-                        NewTask();
-                        break;
-                    default:
-                        {
-                           Console.WriteLine("Please enter 1-4 to complete an action or press 0 to exit.");
-                            break;
-                        }
-                }
-            }while (VariantType )
-
-            Console.Clear();
-            DefaultMenu();
-            exit = Console.ReadLine();
-
-        }
-
-        static void DefaultMenu()
-        {
-            Console.WriteLine("1. Work on a task");
-            Console.WriteLine("2. Complete a task");
-            Console.WriteLine("3. Skip task");
-            Console.WriteLine("4. New task");
-            Console.WriteLine("");
-            Console.Write("Enter your selection:");
-        }
-
-        private static void NewTask()
-        {
-            Console.WriteLine("Test");
-        }
-
-        private static void Skip()
-        {
-            Console.WriteLine("Test");
-        }
-
-        private static void CrossOut()
-        {
-            Console.WriteLine("Test");
-        }
-
-        static void ActionItem()
-        {
-            Console.Clear();
-
-            Console.WriteLine("1. Complete task");
-            Console.WriteLine("2. Incomplete");
-            Console.WriteLine("3. Enter a new task");
-            Console.WriteLine("4. Main Menu");
-            Console.WriteLine("");
-
-            Console.Write("Select the action you would like to complete:");
-
-            string choice = Console.ReadLine();
-            do
-            {
-
-                var quit = false;
-
-                switch (choice)
-                {
-                    case "1":
-                        Console.WriteLine("1. Complete");
-                        break;
-                    case "2":
-                        Console.WriteLine("2. Incomplete");
-                        break;
-                    case "3":
-                        Console.WriteLine("3. Enter a new task");
-                        
-                       
-                        break;
-                    case "4":
-                        Console.WriteLine("4. Main Menu");
-                        //Console.Clear();
-                        DefaultMenu();
-                        break;
-
-                    default:
-                        {
-                            Console.WriteLine("Please enter a number 1-3");
-                            Console.Clear();
-                            ActionItem();
-                            break;
-                        }
-
-                }
-
-            } while (choice != "0");
-
+            
         }
 
         private static void AddTasks(List<string> tasks)
@@ -141,17 +34,23 @@ namespace Tracking_App_2._0
                 AddTasks(tasks);
 
             } while (true);
+
+            WriteListToFile(tasks);
         }
+
+
 
         private void PrintTasks(List<string> tasks)
         {
             foreach (var t in tasks)
             {
-                Console.WriteLine(tasks);
+                Console.WriteLine(t);
             }
+
+            Console.WriteLine();
         }
 
-        private List<string> ReadListFromFile()
+        st List<string> ReadListFromFile()
         {
             var tasks = new List<string>();
 
@@ -163,5 +62,19 @@ namespace Tracking_App_2._0
 
             return tasks;
         }
+
+        static void WriteListToFile(List<string> tasks)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C: \Users\perry\OneDrive\Desktop\Task Project\TaskList.txt"))
+            {
+                foreach (var t in tasks)
+                {
+                    sw.WriteLine(t);
+                }
+            }
+
+        }
     }
+
 }
+
