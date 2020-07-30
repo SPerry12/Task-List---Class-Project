362bf9219f75eb9a374ba9eaaa90888e4226c03c
Shane <perrysm98@gmail.com>
2020-07-30 14:11:21 -0500

    Final commit

--- Tracking App 2.0/Tracking App 2.0/Program.cs
+++ Tracking App 2.0/Tracking App 2.0/Program.cs
@@ -11,58 +11,14 @@ namespace Tracking_App_2._0
     class Program
     {
         
-        
+
         static void Main(string[] args)
         {
-            List<string> taskList = ReadListFromFile();
-            PrintTaskList(taskList);
-
-            var quit = false;
-
-            do
-            {
-                PrintTaskList(taskList);
-                InputTaskToList(taskList);
-            } while (!quit);
+            new App().Run();
+          
         }
 
-        private static void InputTaskToList(List<string> taskList)
-        {
-            Console.WriteLine("Add a new task: ");
-
-            var input = Console.ReadLine();
-            taskList.Add(input);
-        }
-
-        private static void PrintTaskList(List<string> taskList)
-        {
-            foreach (var t in taskList)
-            {
-                Console.WriteLine(t);
-            }
-        }
-
-        private static List<string> ReadListFromFile()
-        {
-            var taskList = new List<string>();
-
-
-            try
-            {
-                using (StreamReader sr = new StreamReader(@"C:\Users\perry\OneDrive\Desktop\Task Project\Task Project.txt"))
-                {
-                    var input = sr.ReadLine();
-                    taskList.Add(input);
-                }
-
-               
-            }
-            catch (FileNotFoundException)
-            { ; }
-
-            return taskList;
-
-        }
+        
     }
 
 }
